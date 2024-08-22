411673d4f3bd8363222fa0735cc38470ffb14ff4
shubu <shubu01001@gmail.com>
2024-08-22 23:19:37 +0530

    bmt calculator created

--- 2-BMICalculator/chaiaurcode.js
+++ 2-BMICalculator/chaiaurcode.js
@@ -1,14 +1,26 @@
-const form = document.querySelector("form");
+const form = document.querySelector('form')
 
-form.addEventListener("submit", (submit) => {
-	submit.preventDefault();
-	const height = parseInt(document.querySelector("#height").value);
-	const weight = parseInt(document.querySelector("#weight"));
+form.addEventListener('submit', (e) => {
+	e.preventDefault()
+	const height = parseInt(document.querySelector('#height').value);
+	const weight = parseInt(document.querySelector('#weight').value);
+	const result = document.querySelector('#results');
 
-	const heightInM = height * (1 / 100);
-
-	function BMI() {
-		weight * heightInM ** 2;
+	if (height === '' || height < 0 || isNaN(height)) {
+		result.innerHTML = `Please enter the valid height ${height}`
+	} else if (weight === '' || weight < 0 || isNaN(weight)) {
+		result.innerHTML = `Please enter the valid weight ${weight}`
+	} else {
+		const bmi = (weight / ((height ** 2) / 10000)).toFixed(2)
+		if (bmi < 18.6) {
+			result.innerHTML = `${bmi} You are Under Weight`
+		} else if (18.6 < bmi & bmi < 24.9) {
+			result.innerHTML = `${bmi} You are Normal Range`
+		} else if (bmi > 24.9) {
+			result.innerHTML = `${bmi} You are Over Weight`
+		}
 	}
-	const results = document.querySelector("#results");
-});
+
+
+
+})
