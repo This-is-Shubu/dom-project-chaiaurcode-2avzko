004688a2fd2c7644d02c74bc79a4e4aecef7a9c2
shubham <shubu01001@gmail.com>
2024-09-18 13:59:17 +0530

    keyboard project completed

--- 5-keyboard/chaiaurcode.js
+++ 5-keyboard/chaiaurcode.js
@@ -3,5 +3,24 @@ console.log("Project 5");
 const insert = document.getElementById("insert");
 
 window.addEventListener("keydown", (e) => {
-	i;
+	insert.innerHTML = `
+	<div class="color">
+	<table>
+		<thead>
+			<tr>
+				<th>Key Code</th>
+				<th>Key Value</th>
+				<th>Key Name</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>${e.keyCode}</td>
+				<td>${e.key === " " ? "Space" : e.key}</td>
+				<td>${e.code}</td>
+			</tr>
+		</tbody>
+	</table>
+	</div>
+	`;
 });
